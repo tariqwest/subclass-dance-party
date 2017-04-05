4429646d9560b426d21f8d2d25ac9122e2d37b3d
Hack Reactor Student <student@hackreactor.com>
2017-04-04 22:39:19 -0700

    Update formatting and spacing

--- es6/src/init.js
+++ es6/src/init.js
@@ -1,31 +1,16 @@
 $(document).ready(function() {
   window.dancers = [];
+  var dancerSound;
 
   $('.addDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
-
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
     var dancerMakerFunctionName = $(this).data('dancer-type');
     
-    
-    // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
-    // make a dancer with a random position
     var topHeight = $('h1').height() + $('.topbar').height();
     var dancer = new dancerMakerFunction(
-      Math.random() * ($('body').height() - topHeight) + topHeight,
-      $("body").width() * Math.random(),
+      Math.random() * ($('body').height() - topHeight) + (topHeight-30),
+      ($("body").width() - 20) * Math.random(),
       1000
     );
     dancer.setPosition(dancer.top, dancer.left);
@@ -33,24 +18,37 @@ $(document).ready(function() {
     $('body').append(dancer.$node);
     window.dancers.push(dancer);
     dancer.step();
-
   });
 
   $('.lineupDancerButton').on('click', function(event) {
+      if(dancerSound){
+        dancerSound.pause();
+      }
+      if($('.unpaired-x')){
+        $('body').append($('.unpaired-x').find('.dancer'));
+        $('.unpaired-x').remove();
+      }
       var top = $("body").height() / 2;
-      var left = 50;
+      var left = ($("body").width() / 2) - (window.dancers.length * 50);
       for(var i=0; i<window.dancers.length; i++){
-        //window.dancers[i].setPosition(top, left);
         window.dancers[i].lineUp(top, left);
         left += 100;
       }
     });
 
   $('.pairDancerButton').on('click', function(event) {
+    if(dancerSound){
+      dancerSound.pause();
+    }
+    if($('.unpaired-x')){
+      $('body').append($('.unpaired-x').find('.dancer'));
+      $('.unpaired-x').remove();
+    }
+    window.dancers.sort(function(a, b){return 0.5 - Math.random()});
     for (var i = 0; i < window.dancers.length - 1; i+=2) {
       var topHeight = $('h1').height() + $('.topbar').height();
-      var randomTop = Math.random() * ($('body').height() - topHeight) + topHeight;
-      var randomLeft = Math.random() * $('body').width();
+      var randomTop = Math.random() * ($('body').height() - topHeight) + (topHeight-30);
+      var randomLeft = Math.random() * ($('body').width() - 20);
       window.dancers[i].setPosition(randomTop, randomLeft);
       window.dancers[i + 1].setPosition(randomTop + 30, randomLeft + 30);
     }
@@ -62,39 +60,30 @@ $(document).ready(function() {
       $($xPath).append($yPath);
       $('body').append($xPath);
 
-      dancerSound = new Audio($($unpaired).data('sound') + '.wav');
-
+      dancerSound = new Audio($($unpaired).data('sound') + '.mp3');
       dancerSound.addEventListener('ended', function() {
         this.currentTime = 0;
         this.play();
       }, false);
-
       dancerSound.play();
-      } 
+    } 
   });
-
-
-  var dancerSound;
 
   $('body').on('mouseenter', '.dancer', function() {
     $(this).toggleClass('enlarge');
-
-    dancerSound = new Audio($(this).data('sound') + '.wav');
-
+    //dancerSound.pause();
+    dancerSound = new Audio($(this).data('sound') + '.mp3');
     dancerSound.addEventListener('ended', function() {
       this.currentTime = 0;
       this.play();
     }, false);
-
     dancerSound.play();
   });
 
   $('body').on('mouseleave', '.dancer', function() {
-    $(this).toggleClass('enlarge');
     dancerSound.pause();
+    $(this).toggleClass('enlarge');
   });
-
-
 
 });
 
